69de35bb77d2497dbdb736030d60c9fb2ab1ce9c
francelino <fmarcilio1989@gmail.com>
2022-09-26 17:42:13 -0300

    feat add components card label and pricing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CardComponent } from './components/card/card.component';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { CardLabelComponent } from './components/card/card-label/card-label.component';
+import { CardPricingComponent } from './components/card/card-pricing/card-pricing.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, CardComponent, MenuBarComponent],
+  declarations: [AppComponent, HomeComponent, CardComponent, MenuBarComponent, CardLabelComponent, CardPricingComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
